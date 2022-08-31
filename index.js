8505bcffb54e8035fe96031b8b2327f4d20ea58b
Acle <99556572+Acle23@users.noreply.github.com>
2022-08-31 03:06:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const navId = document.getElementById("nav-menu"),
+ToggleBtnId = document.getElementById("toggle-btn"),
+CloseBtnId = document.getElementById("close-btn");
+
+ToggleBtnId.addEventListener("click", () => {
+    navId.classList.add("show");
+});
+CloseBtnId.addEventListener("click", () => {
+    navId.classList.remove("show");
+})
